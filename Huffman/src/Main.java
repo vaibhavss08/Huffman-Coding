ba72cba1b336b58c64e989354a3bfddcab2be204
vaibhavss08 <81666621+vaibhavss08@users.noreply.github.com>
2021-08-13 02:13:23 +0530

    Update Main.java

--- Huffman/src/Main.java
+++ Huffman/src/Main.java
@@ -1,76 +1,28 @@
-
-import java.io.*;
 import java.util.*;
 
-public class Main {
+class Main {
 	
-	static HashMap<Character, String> encode = new HashMap<>();
-	int bitLength;
 	public static void main (String [] args) throws Exception {
-		Scanner scan = new Scanner(System.in);
-		StringBuilder s = new StringBuilder();
-		System.out.print("Enter file path : ");
-		String path = scan.nextLine();
-		try {
+		try (Scanner scan = new Scanner(System.in)) {
+			System.out.println("WELCOME!");
 			
-			BufferedReader br = new BufferedReader(new FileReader(path));
-	        int character;
-	        char convert;
-	        while ((character = br.read()) != -1) {
-	            convert = ((char) character);
-	            s.append(convert);
-	        }
-	        
-	        br.close();
-		 } catch (IOException e) {
-		      System.out.println("An error occurred.");
-		      e.printStackTrace();
-		 }
-		
-		HuffmanNode root = new HuffmanNode();
-		int n = s.length();
-		HashMap<Character,Integer> map = new HashMap<>();
-        for(int i=0;i<n;i++) {
-	        	if(map.containsKey(s.charAt(i))) {
-	        		map.put(s.charAt(i),map.get(s.charAt(i))+1);
-	        	}else {
-	        		map.put(s.charAt(i),1);
-	        	}
-        }
-        
-        Set<Character> keys=map.keySet();
-        PriorityQueue<HuffmanNode> q = new PriorityQueue<HuffmanNode>(n, (l1,l2)->l1.frequency-l2.frequency);
- 
-        for (char ch : keys) {
-            HuffmanNode hn = new HuffmanNode();
-            hn.c = ch;
-            hn.frequency = map.get(ch);
-            hn.left = null;
-            hn.right = null;
-            q.add(hn);
-        }
- 
-        while (q.size() > 1) {
-            HuffmanNode x = q.remove();
-            HuffmanNode y = q.remove();
-            HuffmanNode f = new HuffmanNode();
-            f.frequency = x.frequency + y.frequency;
-            f.c = '`';
-            f.left = x;
-            f.right = y;
-            root = f;
-            q.add(f); 
-        }
-        
-        System.out.print("Enter file path to store compressed file : ");
-        String compress_path = scan.nextLine();
-        
-        StringBuilder enc = Encode.encode(compress_path,root, s, encode); // As the method is static can access it without creating object
-        int bitLength=enc.length();
-        
-        System.out.print("Enter file path to decompressed file : ");
-        String decompress_path = scan.nextLine();
-        File file2 = new File(decompress_path);
-        Decode.decompress(file2, encode,bitLength);
+			System.out.println("Want do you want to do?");
+			System.out.println("1. Compression");
+			System.out.println("2. Decompression");
+			System.out.print("Enter your choice : ");
+			int choice = scan.nextInt();
+			switch(choice) {
+				case 1 : 
+					Encode.encode(); // As the method is static can access it without creating object
+					break;
+				case 2 :
+					Decode.decompress();
+					break;
+				default : System.out.println("Incorrect Choice."); 
+				
+			}
+			
+		}
 	}
 }
+
